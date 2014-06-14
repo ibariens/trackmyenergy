19a842bf86ab9f310225fb35b9ca242b73f46296
ibariens <bart.ariens@divimove.com>
2014-06-14 16:55:49 +0200

    allow origin header

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -3,7 +3,7 @@ class DataController < ApplicationController
     @response_data = EnergyLog.create_recent_data.to_json
 
     respond_to do |format|
-      format.json { render :json => @response_data }
+      format.all { render :json => @response_data }
     end
   end
 end
