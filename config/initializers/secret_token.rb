6805c71918cfc1c1229d8b0fcce22b76c8309288
ibariens <bart.ariens@divimove.com>
2014-06-14 16:02:12 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trackmyenergy::Application.config.secret_key_base = 'c1af9f6364bec298b7940cb769e8f53d37b0f95fab189e8b822920ec1fc00b20debb1cc1743276e2516dbd739103ab804f25109dd1af42b05dff041fe6a40490'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Trackmyenergy::Application.config.session_store :cookie_store, key: '_trackmyenergy_session'
