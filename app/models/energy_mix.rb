3dea220e2fabbdc4c09d5cf71744d0f0611c0b09
ibariens <bart.ariens@divimove.com>
2014-06-15 12:07:26 +0200

    sdfsdf

--- app/models/energy_mix.rb
+++ app/models/energy_mix.rb
@@ -6,8 +6,8 @@ class EnergyMix < ActiveRecord::Base
     actual_usage = self.first
 
     last_usage = actual_usage.real_consumption
-    wind = actual_usage.wind_share
-    sun = actual_usage.solar_share
+    wind = (actual_usage.wind_share*100).round(2)
+    sun = (actual_usage.solar_share*100).round(2)
     fire = 100 - wind - sun
     c02_saved = actual_usage.co2_saved
     eur_saved = actual_usage.eur_saved
