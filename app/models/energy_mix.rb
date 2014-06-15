76b8e92342e95266e2db552dd7620513f990a499
ibariens <bart.ariens@divimove.com>
2014-06-15 12:09:08 +0200

    rounding

--- app/models/energy_mix.rb
+++ app/models/energy_mix.rb
@@ -6,18 +6,18 @@ class EnergyMix < ActiveRecord::Base
     actual_usage = self.first
 
     last_usage = actual_usage.real_consumption
-    wind = (actual_usage.wind_share*100).round(2)
-    sun = (actual_usage.solar_share*100).round(2)
-    fire = 100 - wind - sun
+    wind = (actual_usage.wind_share*100)
+    sun = (actual_usage.solar_share*100)
+    fire = (100 - wind - sun)
     c02_saved = actual_usage.co2_saved
     eur_saved = actual_usage.eur_saved
 
 
-    {:current_usage => {:energy => last_usage,
-                        :wind => wind,
-                        :sun => sun,
-                        :fire => fire,
-                        :co_two_saved => c02_saved,
-                        :eur_saved => eur_saved}}
+    {:current_usage => {:energy => last_usage.round(2),
+                        :wind => wind.round(2),
+                        :sun => sun.round(2),
+                        :fire => fire.round(2),
+                        :co_two_saved => c02_saved.round(2),
+                        :eur_saved => eur_saved.round(2)}}
   end
 end
