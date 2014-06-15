9a37dc3664d67399bbd94d69204ccf1ac9c4ca79
ibariens <bart.ariens@divimove.com>
2014-06-15 04:33:30 +0200

    sdfsdf

--- app/models/appliance.rb
+++ app/models/appliance.rb
@@ -2,5 +2,15 @@ class Appliance < ActiveRecord::Base
   def self.get_device_list
     return [['Vacuum Clear', 234],['FAN', 3423423]]
   end
+
+  def self.fetch_name(id)
+    name = 'unknown'
+    pattern = Pattern.where("pattern_id = ?", id.to_s).first
+    if pattern
+      appliance = Appliance.find(pattern.appliance_id)
+      name = appliance.name if appliance
+    end
+    name
+  end
 end
 
