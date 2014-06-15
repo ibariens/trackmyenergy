277c8ab2d2e1ea374d268a41c2d42f1c1a6584ab
ibariens <bart.ariens@divimove.com>
2014-06-15 04:35:49 +0200

    asdasd

--- app/models/pattern.rb
+++ app/models/pattern.rb
@@ -1,15 +1,15 @@
 class Pattern < ActiveRecord::Base
   def self.define_action(status_message)
     status_array = status_message.split(' ')
-    if status_array.include? 'Request identificatio'
-      {:action => {:message => status_message,
-                   :pattern_id => status_array[2],
-                   :device_choice => Appliance.get_device_list}}
-    elsif status_array.include? 'Identified and Turned'
+    if status_array.include? 'Identified and Turned'
       {:action => {:message => status_message,
                    :pattern_id => status_array[2],
                    :appliance_name => Appliance.fetch_name(status_array[2]),
                    :device_choice => Appliance.get_device_list}}
+    else status_array.include? 'Request identification'
+        {:action => {:message => status_message,
+                     :pattern_id => status_array[2],
+                     :device_choice => Appliance.get_device_list}}
     end
   end
 end
