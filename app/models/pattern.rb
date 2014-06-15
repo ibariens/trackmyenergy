6a2eaaa302185005fbc628bc9b886f835e5de7ef
ibariens <bart.ariens@divimove.com>
2014-06-15 04:16:13 +0200

    fixes small bug

--- app/models/pattern.rb
+++ app/models/pattern.rb
@@ -2,7 +2,7 @@ class Pattern < ActiveRecord::Base
   def self.define_action(status_message)
     status_array = status_message.split(' ')
     if status_array.include? 'Pattern'
-      {:action => {:message => 'status_message',
+      {:action => {:message => status_message,
                    :pattern_id => status_array[2],
                    :device_choice => Appliance.get_device_list}}
     end
